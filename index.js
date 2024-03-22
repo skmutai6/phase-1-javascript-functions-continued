dcf87bc2f57ac45967b0f3c3965371024f287934
skmutai6 <skmutai6@gmail.com>
2024-03-22 15:19:35 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+
+function saturdayFun(){
+    function saturdayFun(){
+        let saturdayFun = "This Saturday, I want to roller-skate!";
+    }
+}
+
+function mondayWork(){
+    let mondayWork = "This Monday, I will go to the office.";
+}
+
+function wrapAdjective(){
+}
+
+
+
